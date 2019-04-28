6946e372c99a4b4768f520616f5531ad1b7ab87d
Srikanth Kamath <Srikanth@tskamath.com>
2019-04-28 20:42:12 +0530

    basic configuration

---
+++ footer.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package DepWP
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer row">
+		<div class="site-info col-md-12">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'depwp' ) ); ?>">
+				<?php
+				/* translators: %s: CMS name, i.e. WordPress. */
+				printf( esc_html__( 'Proudly Powered by %s', 'depwp' ), 'WordPress' );
+				?>
+			</a>
+			<span class="sep"> | </span>
+				<?php
+				/* translators: 1: Theme name, 2: Theme author. */
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'depwp' ), 'DepWP', '<a href="http://www.tskamath.com">Tskamath Technologies</a>' );
+				?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ inc/widgets.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Register widget area.
+ *
+ * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
+ */
+function depwp_widgets_init() {
+	register_sidebar( array(
+		'name'          => esc_html__( 'Right Sidebar', 'depwp' ),
+		'id'            => 'right-sidebar',
+		'description'   => esc_html__( 'Add widgets here.', 'depwp' ),
+		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</section>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
+	) );
+}
+add_action( 'widgets_init', 'depwp_widgets_init' );
